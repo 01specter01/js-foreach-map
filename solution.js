827d03748aac52dfffffdcbaaf28e70bb08ad699
Leo Volwahsen <volwahsenl@gmail.com>
2022-08-17 13:49:48 +0200

    2 done

---
+++ solution.js
@@ -0,0 +1,26 @@
+console.log("Aufgabe1");
+const array = [1, 2, 3, 4, 5, 6];
+function doubleValues(array) {
+    let newArray = [];
+    array.forEach(function (i) {
+        newArray.push(i, i);
+    });
+    return newArray;
+}
+console.log(doubleValues(array));
+
+console.log("Aufgabe2");
+const list = [1, 2, 3];
+function onlyEvenValues(list) {
+    let evenArray = [];
+    list.forEach(function (i) {
+        if (i % 2 == 0) evenArray.push(i);
+    });
+    return evenArray;
+}
+console.log(onlyEvenValues(list));
+
+console.log("Aufgabe3");
+const zeichenkette = ["j", 5, "!", 3];
+function showFirstAndLast(zeichenkette) {}
+console.log(showFirstAndLast(zeichenkette));
